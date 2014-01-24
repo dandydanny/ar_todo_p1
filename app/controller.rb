9c412d0dfd32217ad2fcc792b5c60b0f69fa303e
Apprentice <apprentice@dbc-chicago-73.local>
2014-01-24 15:54:16 -0600

    Finished controller delete.

--- app/controller.rb
+++ app/controller.rb
@@ -34,7 +34,7 @@ class Controller
     if ARGV[1] == "list"
       display_list
     elsif ARGV[1] == "lists"
-      display_all_list
+      display_all_lists
     else
       raise "Please use 'display list' or 'display lists'."
     end
@@ -52,6 +52,13 @@ class Controller
   def sort_delete_args
     if ARGV[1] == "task" && ARGV.size == 3
       Task.destroy(ARGV[2])
+      display_list
+    elsif ARGV[1] == "list"
+      List.find(ARGV[2].to_i).destroy
+    else
+      raise "Please use correct magical syntax. (DO ETTT.)"
+    end
+  end
 
 
   def display_list
