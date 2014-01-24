c2048f0eda1a8ff725c1fa011e15b52ea340f721
Apprentice <apprentice@dbc-chicago-80.local>
2014-01-24 15:01:39 -0600

    Added fancy List call back on before_destroy (WE ARE AWESOME) and display all list method.

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,4 +5,17 @@ class List < ActiveRecord::Base
 
   validates :name, presence: { :message => 'OOHHH HECK NO WE NEED A LIST NAME!' }
 
+  # before_destroy { |record| Task.destroy_all "list_id = #{self.id}" }
+  before_destroy 'Task.destroy_all "list_id = #{self.id}"'
+
+  def self.all_lists #display ALL lists
+    all_lists = ""
+    num = 1
+    self.all.each do |list|
+      all_lists += "#{num}. #{list.name}\n"
+      num += 1
+    end
+    all_lists
+  end
+
 end
