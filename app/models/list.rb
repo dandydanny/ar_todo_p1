87bc2425c7d5b1002bb19829ff4255b40fe48e3c
Apprentice <apprentice@dbc-chicago034.local>
2014-01-24 11:13:50 -0600

    Added models, and validation to models.

---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+require 'active_record'
+
+class List < ActiveRecord::Base
+  has_many :tasks
+
+  validates :name, presence: { :message => 'OOHHH HECK NO WE NEED A LIST NAME!' } 
+
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
+require 'active_record'
+
 class Task < ActiveRecord::Base
+  belongs_to :list
+
+  validates :description, presence: { :message => 'OOHHH HECK NO WE NEED A TASK DESCRIPTION!' } 
+
 end
