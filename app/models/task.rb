d31027c5cf74ae85170854aced8cf49128426a5f
Apprentice <apprentice@dbc-chicago-73.local>
2014-01-24 14:26:16 -0600

    Added method to set completed-ness

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,26 @@
 require 'active_record'
 
 class Task < ActiveRecord::Base
+
+  after_initialize :init
+
   belongs_to :list
 
-  validates :description, presence: { :message => 'OOHHH HECK NO WE NEED A TASK DESCRIPTION!' } 
+  validates :description, presence: { :message => 'OOHHH HECK NO WE NEED A TASK DESCRIPTION!' }
+  def init
+    self.completed ||= false
+  end
 
+  def complete
+    self.update(completed: true)
+  end
+
+  def incomplete
+    self.update(completed: false)
+  end
+
+  def to_s
+    done = self.completed ? "X" : " "
+    "#{self.id}. [#{done}]  #{self.description}"
+  end
 end
