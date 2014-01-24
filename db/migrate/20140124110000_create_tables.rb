b374ef94e8fbaf82a6f003297252b2ea37ec0405
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-24 11:24:19 -0600

    Created initial tables, "lists", "tasks" for DB migration.

---
+++ db/migrate/20140124110000_create_tables.rb
@@ -0,0 +1,21 @@
+require_relative '../config'
+
+# this is where you should use an ActiveRecord migration to 
+
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.integer :task_id
+      t.boolean :completed
+      t.string  :description
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+
+    create_table :lists do |t|
+      t.string :name
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
